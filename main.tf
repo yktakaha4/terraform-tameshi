1f5c2976f94230008f9ddba6568d08e7f08435b6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-15 22:45:35 +0900

    add sample

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "Otameshi"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
